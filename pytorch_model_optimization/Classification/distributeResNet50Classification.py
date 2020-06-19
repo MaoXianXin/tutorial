c23a9ea97e256c0a103e85f7e24f93a44f48552b
XianxinMao <qq1044467857@gmail.com>
2020-06-19 21:37:37 +0800

    pytorch分布式resnet50

--- pytorch_model_optimization/Classification/distributeResNet50Classification.py
+++ pytorch_model_optimization/Classification/distributeResNet50Classification.py
@@ -8,6 +8,7 @@ import numpy as np
 from autoaugment import ImageNetPolicy
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# torch.backends.cudnn.benchmark = True
 print(device)
 
 num_classes = 101
@@ -16,7 +17,7 @@ data_dir = './data/food-101'
 train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                        transforms.RandomResizedCrop(224),
                                        transforms.RandomHorizontalFlip(),
-                                       ImageNetPolicy(),
+                                       # ImageNetPolicy(),
                                        transforms.ToTensor(),
                                        transforms.Normalize([0.485, 0.456, 0.406],
                                                             [0.229, 0.224, 0.225])])
@@ -33,8 +34,8 @@ test_data = datasets.ImageFolder(data_dir + '/valid', transform=test_transforms)
 
 """当num_workers>0时,它会自动预读取最多2个batch的数据,只要CPU,硬盘处理得足够快,就会形成1个送入model进行处理,1个在队列中等待,1个新的正在预处理的状态,这种情况下加prefetch是没用的"""
 """DataLoader早就加入了多进程读取机制,就是那个num_worker选项,所以完全没必要prefetch了"""
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=192, shuffle=True, num_workers=4, pin_memory=True)
-testloader = torch.utils.data.DataLoader(test_data, batch_size=192, num_workers=4, pin_memory=True)
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=320, shuffle=True, num_workers=8, pin_memory=True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=320, num_workers=8, pin_memory=True)
 print(len(trainloader))
 
 model = models.resnet50(pretrained=True)
@@ -45,7 +46,6 @@ model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-3, betas=[0.9, 0.999])
-
 
 def train(n_epochs, trainloader, testloader, resnet, optimizer, criterion, save_path, scheduler):
     """returns trained model"""
@@ -74,33 +74,34 @@ def train(n_epochs, trainloader, testloader, resnet, optimizer, criterion, save_
         # print(f"Device = cuda; Time per epoch: {(end - start):.3f} seconds")
 
         # 开始一个epoch的推断
-        resnet.eval()
-        valid_loss = 0
-        accuracy = 0
-        with torch.no_grad():
-            for inputs, labels in testloader:
-                inputs, labels = inputs.to(device), labels.to(device)
-                logits = resnet(inputs)
-                batch_loss = criterion(logits, labels)
-                valid_loss += batch_loss.item()
-
-                # Calculate accuracy
-                top_p, top_class = logits.topk(1, dim=1)
-                equals = top_class == labels.view(top_class.shape)
-                accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-
-            if valid_loss <= valid_loss_min:
-                print("Validation loss decreased Saving model")
-                torch.save(resnet.state_dict(), save_path)
-                valid_loss_min = valid_loss
+        # resnet.eval()
+        # valid_loss = 0
+        # accuracy = 0
+        # with torch.no_grad():
+        #     for inputs, labels in testloader:
+        #         inputs, labels = inputs.to(device), labels.to(device)
+        #         logits = resnet(inputs)
+        #         batch_loss = criterion(logits, labels)
+        #         valid_loss += batch_loss.item()
+        #
+        #         # Calculate accuracy
+        #         top_p, top_class = logits.topk(1, dim=1)
+        #         equals = top_class == labels.view(top_class.shape)
+        #         accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+        #
+        #     if valid_loss <= valid_loss_min:
+        #         print("Validation loss decreased Saving model")
+        #         torch.save(resnet.state_dict(), save_path)
+        #         valid_loss_min = valid_loss
 
         end = time.time()
         print(f"Device = cuda; Time per epoch: {(end - start):.3f} seconds")
-        print(f"Epoch {epoch+1}/{n_epochs}.. "
+        print(f"Epoch {epoch}/{n_epochs}.. "
               f"Train loss: {running_loss / len(trainloader):.3f}.. "
-              f"Test loss: {valid_loss / len(testloader):.3f}.. "
-              f"Test accuracy: {accuracy / len(testloader):.3f}")
-        resnet.train()
+              # f"Test loss: {valid_loss / len(testloader):.3f}.. "
+              # f"Test accuracy: {accuracy / len(testloader):.3f}"
+              )
+        # resnet.train()
 
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=13, gamma=0.1)
 train(30, trainloader, testloader, model, optimizer, criterion, 'food_classifier_resnet50_noise.pth', exp_lr_scheduler)
